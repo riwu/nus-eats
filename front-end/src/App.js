5971ce14a18c358b9edf7929a7bf7aebd545d6fb
riwu <riwu0730@gmail.com>
2017-08-19 08:50:18 +0800

    add eslint and basic config

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import MainApp from './components/App';
+import reducer from './reducers';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const store = createStore(reducer);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <MainApp />
+    </Provider>
+  );
 }
 
 export default App;
---
+++ front-end/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+function reducer(state = 0, action) {
+  return state;
+}
+
+const app = combineReducers({
+  reducer,
+});
+
+export default app;
