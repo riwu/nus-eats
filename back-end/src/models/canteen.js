6c8a5897af15f949f714c3bbc4acbb398f514a0e
Yihang Ho <hoyihang5@gmail.com>
2017-08-20 12:16:23 +0800

    Update canteens schema

--- back-end/src/models/canteen.js
+++ back-end/src/models/canteen.js
@@ -5,6 +5,14 @@ const Canteen = sequelize.define('canteens', {
   name: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  latitude: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  longitude: {
+    type: Sequelize.FLOAT,
+    allowNull: false
   }
 });
 
