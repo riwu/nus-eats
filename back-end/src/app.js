5fe2aa6e1ab87aeef54554a53e1d008c6a337c67
Charlton Lim <cadmusthefounder@gmail.com>
2017-08-22 17:26:42 +0800

    Remove useless dependencies and folders

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -54,8 +54,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({errors: [ "Unknown error." ]});
 });
 
 module.exports = app;
