31cdd42c47dee9a9abf3ee56247fe940c24b7cbd
Charlton Lim <cadmusthefounder@gmail.com>
2017-08-27 14:13:18 +0800

    Add update and delete appintments

--- back-end/src/routes/appointments.js
+++ back-end/src/routes/appointments.js
@@ -12,7 +12,38 @@ module.exports = (db) => {
       canteen_id: req.body.appointment.canteen_id
     })
     .then(appointment => res.status(201).json({appointment}));
-  })
+  });
+
+  router.patch('/:appointmentId', (req, res, next) => {
+    db['Appointment']
+    .findById(req.params.appointmentId)
+    .then(appointment => {
+      if (appointment.user_id == req.user.id) {
+        res.send(403);
+      }
+  
+      appointment.attendees.forEach(attendee => {
+        if (attendee == req.user.id) {
+          res.status(409);
+        }
+      });
+
+      appointment.attendees.push(req.user.id);
+      return appointment.update({attendees: appointment.attendees});
+    }).then(appointment => {
+      res.json({appointment});
+    });
+  });
+
+  router.delete('/:appointmentId', (req, res, next) => {
+    db['Appointment']
+    .destroy({
+      where: { id: req.params.appointmentId }
+    })
+    .then(response => {
+      res.json({response});
+    });
+  });
 
   return router;
 }
