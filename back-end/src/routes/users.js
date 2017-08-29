fa8fd7cfd1e8cb9fc87d2fd1bdccda245feae399
Charlton Lim <cadmusthefounder@gmail.com>
2017-08-29 18:00:26 +0800

    fix underscore bug

--- back-end/src/routes/users.js
+++ back-end/src/routes/users.js
@@ -5,7 +5,7 @@ const asyncMiddleware = require('../utilities/async');
 module.exports = (db) => {
     router.get('/:userId/appointments', asyncMiddleware(async (req, res, next) => {
       const appointments = await db['appointment'].findAll({
-        where: { user_id: req.params.userId }
+        where: { userId: req.params.userId }
       });
       res.json({appointments});
     }));
