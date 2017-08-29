61b06978530336205a01e90b620e301f98795bae
riwu <riwu0730@gmail.com>
2017-08-29 23:43:35 +0800

    Refactor reducer file names from underscore to camelcase rename toggleMuslimOnly to toggleHalalOnly reame expandedCanteenPanels to expandedCanteenPanel

--- front-end/src/containers/CanteenPanelContainer.js
+++ front-end/src/containers/CanteenPanelContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import CanteenPanel from '../components/CanteenPanel';
 
 const mapStateToProps = (state, ownProps) => ({
-  expanded: state.expandedCanteenPanels === ownProps.canteenId,
+  expanded: state.expandedCanteenPanel === ownProps.canteenId,
   header: ownProps.header,
   children: ownProps.children,
 });
