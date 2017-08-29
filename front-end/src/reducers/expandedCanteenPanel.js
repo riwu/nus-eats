7e65448aa768aac43fc194b8646d3ca350be0426
riwu <riwu0730@gmail.com>
2017-08-29 23:45:29 +0800

    allow expanded panel to be hidden by clicking on it again

--- front-end/src/reducers/expandedCanteenPanel.js
+++ front-end/src/reducers/expandedCanteenPanel.js
@@ -2,7 +2,7 @@ import { TOGGLE_CANTEEN_PANEL } from '../constants/ActionTypes';
 
 function reducer(state = null, action) {
   if (action.type !== TOGGLE_CANTEEN_PANEL) return state;
-  return action.canteenId;
+  return action.canteenId === state ? null : action.canteenId;
 }
 
 export default reducer;
