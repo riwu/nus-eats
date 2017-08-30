0ee9cdc597fd9389f3b6d95313b41c63af6e8c3c
Yihang Ho <hoyihang5@gmail.com>
2017-08-30 17:14:26 +0800

    Complete feed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import { ConnectedRouter } from 'react-router-redux';
 import { Route } from 'react-router-dom';
 import history from './store/history';
 
-import { getAllCanteens, getAllStalls, getMeetings } from './actions';
+import { getAllCanteens, getAllStalls } from './actions';
 import store from './store';
 
 import FacebookSDK from './containers/FacebookSDKContainer';
@@ -14,7 +14,6 @@ import Feed from './containers/Feed';
 
 store.dispatch(getAllCanteens());
 store.dispatch(getAllStalls());
-store.dispatch(getMeetings());
 
 function App() {
   return (
