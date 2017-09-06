39733e9591277a453c294ae80f5f8a4e14749c53
Yihang Ho <hoyihang5@gmail.com>
2017-09-06 10:42:09 +0800

    Improve local storage

--- front-end/src/store/local_storage/index.js
+++ front-end/src/store/local_storage/index.js
@@ -2,16 +2,36 @@ import { get as getAccessTokens, set as setAccessTokens } from './access_tokens'
 import { get as getCurrentUser, set as setCurrentUser } from './current_user';
 import { get as getGrantedPermissions, set as setGrantedPermissions } from './granted_permissions';
 
-const get = () => ({
-  accessTokens: getAccessTokens(),
-  currentUser: getCurrentUser(),
-  grantedPermissions: getGrantedPermissions(),
-});
+let previousState = {};
+
+const get = () => {
+  const accessTokens = getAccessTokens();
+  const currentUser = getCurrentUser();
+  const grantedPermissions = getGrantedPermissions();
+
+  previousState = {
+    accessTokens,
+    currentUser,
+    grantedPermissions
+  };
+
+  return previousState;
+};
 
 const set = (state) => {
-  setAccessTokens(state.accessTokens);
-  setCurrentUser(state.currentUser);
-  setGrantedPermissions(state.grantedPermissions);
+  if (state.accessTokens !== previousState.accessTokens) {
+    setAccessTokens(state.accessTokens);
+  }
+
+  if (state.currentUser !== previousState.currentUser) {
+    setCurrentUser(state.currentUser);
+  }
+
+  if (state.grantedPermissions !== previousState.grantedPermissions) {
+    setGrantedPermissions(state.grantedPermissions);
+  }
+
+  previousState = state;
 };
 
 
