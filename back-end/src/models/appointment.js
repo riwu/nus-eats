273589597e354c0dbf93f5a232087ae473437dab
Charlton Lim <cadmusthefounder@gmail.com>
2017-09-06 16:53:24 +0800

    change to text

--- back-end/src/models/appointment.js
+++ back-end/src/models/appointment.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false
       },
       description: {
-        type: DataTypes.STRING
+        type: DataTypes.TEXT
       }
     }, {
       paranoid: true
