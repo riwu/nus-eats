db0656d8940007493b0519527ef0120904c3d8a7
Yihang Ho <hoyihang5@gmail.com>
2017-09-08 01:43:14 +0800

    Implement Facebook like

---
+++ front-end/src/components/facebook/FacebookLikeContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import FacebookLike from './FacebookLike';
+
+const mapStateToProps = (state) => ({
+  loading: !state.isFacebookReady
+});
+
+const FacebookLikeContainer = connect(
+  mapStateToProps
+)(FacebookLike);
+
+export default FacebookLikeContainer;
