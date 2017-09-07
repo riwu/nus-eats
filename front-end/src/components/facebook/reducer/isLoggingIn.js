2086556d2584751670416314c78f77bd2011f9aa
Yihang Ho <hoyihang5@gmail.com>
2017-09-07 17:05:25 +0800

    Fix login button issue

--- front-end/src/components/facebook/reducer/isLoggingIn.js
+++ front-end/src/components/facebook/reducer/isLoggingIn.js
@@ -1,6 +1,6 @@
-import { BEGIN_LOGIN, DONE_LOGIN } from '../../../constants/ActionTypes';
+import { BEGIN_LOGIN, DONE_LOGIN, LOGOUT } from '../../../constants/ActionTypes';
 import makeBoolean from '../../../higher_order_reducers/boolean';
 
-const reducer = makeBoolean(false, [BEGIN_LOGIN], [DONE_LOGIN]);
+const reducer = makeBoolean(false, [BEGIN_LOGIN], [DONE_LOGIN, LOGOUT]);
 
 export default reducer;
