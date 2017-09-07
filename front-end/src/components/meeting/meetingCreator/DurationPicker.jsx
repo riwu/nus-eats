99a03f3b0b986f1f3cda0ab3efe091d3ed460cda
riwu <riwu0730@gmail.com>
2017-09-07 16:03:45 +0800

    Use overlay trigger warning instead of disabled button for invalid meeting for mobile friendliness

--- front-end/src/components/meeting/meetingCreator/DurationPicker.jsx
+++ front-end/src/components/meeting/meetingCreator/DurationPicker.jsx
@@ -5,30 +5,28 @@ import './DurationPicker.css';
 
 // have to use index as key as duration can be set to the same
 const DurationPicker = ({ durations, updateDurationModifierRadio, activeDurationModifierIndex }) => (
-  <div>
-    <Form inline>
-      {durations.map((duration, index) => (
-        <div key={index}>
-          <FormGroup>
-            <Radio
-              checked={index === activeDurationModifierIndex}
-              onChange={() => { updateDurationModifierRadio(index); }}
-            />
-            <FormControl
-              className="inputBox"
-              type="number"
-              min={0}
-              value={duration.value ? duration.value.asMinutes() : ''}
-              onChange={event => duration.onUpdate(moment.duration(Number(event.target.value), 'minutes'))}
-              step={10}
-              onClick={() => { updateDurationModifierRadio(index); }}
-            />
-            <ControlLabel>Minutes</ControlLabel>
-          </FormGroup>
-        </div>
+  <Form inline>
+    {durations.map((duration, index) => (
+      <div key={index}>
+        <FormGroup>
+          <Radio
+            checked={index === activeDurationModifierIndex}
+            onChange={() => { updateDurationModifierRadio(index); }}
+          />
+          <FormControl
+            className="inputBox"
+            type="number"
+            min={0}
+            value={duration.value ? duration.value.asMinutes() : ''}
+            onChange={event => duration.onUpdate(moment.duration(Number(event.target.value), 'minutes'))}
+            step={10}
+            onClick={() => { updateDurationModifierRadio(index); }}
+          />
+          <ControlLabel>Minutes</ControlLabel>
+        </FormGroup>
+      </div>
       ))}
-    </Form>
-  </div>
+  </Form>
 );
 
 export default DurationPicker;
--- front-end/src/components/meeting/meetingCreator/Meeting.jsx
+++ front-end/src/components/meeting/meetingCreator/Meeting.jsx
@@ -5,51 +5,61 @@ import MeetingsDisplay from './MeetingsDisplayContainer';
 import getMergedDate from '../../../util/getMergedDate';
 
 const Meeting = ({ toggleMeetingWindow, canteen, isOpen, createMeeting,
-   newMeetingDate, newMeetingTime, newMeetingDuration, title, description }) => (
-     <div>
-       <Button onClick={toggleMeetingWindow}>
-      + Meeting
+   newMeetingDate, newMeetingTime, newMeetingDuration, title, description }) => {
+  const trimmedTitle = title.trim();
+  const button = (
+    <Button
+      bsStyle="primary"
+      onClick={() => {
+        if (trimmedTitle === '') {
+          return;
+        }
+        createMeeting({
+          canteenId: canteen.id,
+          startTime: getMergedDate(newMeetingDate, newMeetingTime),
+          duration: newMeetingDuration,
+          title: trimmedTitle,
+          description,
+        });
+      }}
+    >
+      Create
     </Button>
-       <Modal
-         show={isOpen}
-         onHide={toggleMeetingWindow}
-         animation={false}
-       >
-         <Modal.Header closeButton>
-           <Modal.Title>{canteen ? canteen.name : null}</Modal.Title>
-         </Modal.Header>
-         <Modal.Body>
-           <MeetingCreator />
-           <MeetingsDisplay />
-         </Modal.Body>
-         <Modal.Footer>
-           <OverlayTrigger
-             placement="left"
-             overlay={title.trim() ? '' : <Tooltip id="Enter a title">Enter a title!</Tooltip>}
-           >
-             <div style={{ display: 'inline' }}>
-               <Button
-                 style={{ pointerEvents: 'none' }}
-                 bsStyle="primary"
-                 disabled={!title.trim()}
-                 onClick={() => {
-                   createMeeting({
-                     canteenId: canteen.id,
-                     startTime: getMergedDate(newMeetingDate, newMeetingTime),
-                     duration: newMeetingDuration,
-                     title: title.trim(),
-                     description,
-                   });
-                 }}
-               >
-                Create
-              </Button>
-             </div>
-           </OverlayTrigger>
-           <Button onClick={toggleMeetingWindow}>Close</Button>
-         </Modal.Footer>
-       </Modal>
-     </div>
-);
+  );
+  return (
+    <div>
+      <Button onClick={toggleMeetingWindow}>
+        + Meeting
+      </Button>
+      <Modal
+        show={isOpen}
+        onHide={toggleMeetingWindow}
+        animation={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{canteen ? canteen.name : null}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <MeetingCreator />
+          <MeetingsDisplay />
+        </Modal.Body>
+        <Modal.Footer>
+          {
+            trimmedTitle ? button :
+            <OverlayTrigger
+              ref={(ref) => { this.overlayRef = ref; }}
+              placement="left"
+              overlay={<Tooltip id="Enter a title">Enter a title!</Tooltip>}
+            >
+              {button}
+            </OverlayTrigger>
+          }
+
+          <Button onClick={toggleMeetingWindow}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
 export default Meeting;
