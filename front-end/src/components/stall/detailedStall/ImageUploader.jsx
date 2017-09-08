78488a5778e98a040628a9bc9249706f074f100e
riwu <riwu0730@gmail.com>
2017-09-08 20:11:00 +0800

    validate file upload type

--- front-end/src/components/stall/detailedStall/ImageUploader.jsx
+++ front-end/src/components/stall/detailedStall/ImageUploader.jsx
@@ -7,9 +7,19 @@ import api from '../../../api';
 const ImageUploader = ({ stallId }) => (
   <div className="ImageUpload">
     <ImageUpload
-      onChange={(e, results) =>
-        api.uploadFiles(results.map(([ignore, file]) => file), stallId)
-      }
+      onChange={(e, results) => {
+        const getFileType = file => file.name.substring(file.name.lastIndexOf('.') + 1);
+        const acceptedFileTypes = ['jpeg', 'jpg', 'png', 'gif', 'bmp'];
+        const invalidResult = results.find(([ignore, file]) => !acceptedFileTypes
+            .includes(getFileType(file).toLowerCase()));
+        if (invalidResult) {
+          alert(`Invalid file type: ${getFileType(invalidResult[1])}. Only ${acceptedFileTypes.join(', ')} are accepted.`);
+          return;
+        }
+        api.uploadFiles(results.map(([ignore, file]) => file), stallId);
+      }}
+      accept="image/*"
+      multiple
     >
       <Button bsStyle="primary">
         <Glyphicon glyph="upload" className="uploadGlyph" />
