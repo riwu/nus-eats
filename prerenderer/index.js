428dbb701f657aeafe9ca505ab8a455739f3285a
Yihang Ho <hoyihang5@gmail.com>
2017-09-08 18:53:02 +0800

    Complete FB service

--- prerenderer/index.js
+++ prerenderer/index.js
@@ -5,11 +5,11 @@ const app = express();
 const API_BASE_URL = 'https://api.nuseats.club';
 const canteensPromise = fetch(API_BASE_URL + '/canteens')
   .then((response) => response.json())
-  .then(({canteens}) => canteens);
+  .then(({canteens}) => canteens)
   .then((canteens) => canteens.reduce((obj, canteen) => {
     obj[canteen.id] = canteen;
     return obj;
-  }, {});
+  }, {}));
 
 app.get('/meetings/:id', function(req, res) {
   const id = req.params.id;
@@ -19,8 +19,37 @@ app.get('/meetings/:id', function(req, res) {
     .then(({appointment}) => appointment);
 
   Promise.all([canteensPromise, appointmentPromise])
+    .then(([canteens, appointment]) => {
+      appointment.canteen = canteens[appointment.canteenId] || {};
+
+      const template = `
+      <!doctype html>
+      <html>
+      <head>
+        <meta property="og:type" content="article">
+        <meta property="fb:app_id" content="127260141236030">
+        <meta property="og:url" content="https://fb.nuseats.club/meetings/${appointment.id}">
+        <meta property="og:title" content="${appointment.title}">
+        <meta property="og:description" content="${appointment.description}">
+        <meta property="og:image" content="${appointment.canteen.imageUrl}">
+      </head>
+      <body>
+        <p>
+          Redirecting you to <a href="https://www.nuseats.club/meetings/${appointment.id}">https://www.nuseats.club/meetings/${appointment.id}</a>
+        </p>
+
+        <script>
+          window.location.replace('https://www.nuseats.club/meetings/${appointment.id}');
+        </script>
+      </body>
+      </html>
+      `;
+
+      res.set('Content-Type', 'text/html');
+      res.send(template);
+    });
 });
 
-app.listen(3000, function () {
+app.listen(3001, function () {
   console.log('Running');
 });
