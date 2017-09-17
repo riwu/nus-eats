411d31e456789cb857de3cf2f38615669ed668dc
riwu <riwu0730@gmail.com>
2017-09-17 23:38:58 +0800

    Remove onresize event when component is unmounted

--- front-end/src/components/meeting/meetingCreator/DurationPicker.jsx
+++ front-end/src/components/meeting/meetingCreator/DurationPicker.jsx
@@ -11,11 +11,16 @@ class DurationPicker extends React.Component {
     this.state = {
       verticalSelection: isBigWindow(),
     };
-    window.onresize = () => {
-      console.log(Math.max(document.documentElement.clientWidth, window.innerWidth || 0));
-      this.setState({ verticalSelection: isBigWindow() });
-    };
   }
+
+  componentWillMount() {
+    window.onresize = () => this.setState({ verticalSelection: isBigWindow() });
+  }
+
+  componentWillUnmount() {
+    window.onresize = () => {};
+  }
+
   render() {
     return (
       <ButtonGroup vertical={this.state.verticalSelection} className="DurationPicker">
